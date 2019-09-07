63d739ed7946f18855fc4bbdbffaa9b982d7bdae
banji <olabanjiabimbola@gmail.com>
2019-09-07 11:38:57 +0100

    Initial Commit

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+
+
+class  App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <h1>Event-Nigeria</h1>
+      </div>
+    );
+
+  }
+  
+}
+
+export default App;
+
